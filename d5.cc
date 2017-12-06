7b6cfaf234b675a2d581619ffbb0945a6081dc78
Ke Zhang <ke.zhang@mycase.com>
2017-12-05 22:59:52 -0800

    d5 in c++

---
+++ d5.cc
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+
+#include "d5d.cc"
+
+using namespace std;
+
+int p1(const vector<int> & initial_maze){
+  vector<int> maze(initial_maze);
+  int step = 0;
+  vector<int>::iterator it = maze.begin();
+
+  while (it >= maze.begin() && it < maze.end()) {
+    it += (*it)++;
+    ++step;
+  }
+  return step;
+}
+
+int p2(const vector<int> & initial_maze){
+  vector<int> maze(initial_maze);
+  int step = 0;
+  vector<int>::iterator it = maze.begin();
+
+  while (it >= maze.begin() && it < maze.end()) {
+    it += (*it < 3 ? (*it)++ : (*it)--);
+    ++step;
+  }
+  return step;
+}
+
+int main(){
+  cout << p1(initial_maze) << endl;
+  cout << p2(initial_maze) << endl;
+}

95b5eed23e3de2e2bb62320a8a4b264efde3562f
Ke Zhang <ke.zhang@mycase.com>
2018-11-30 23:45:36 -0800

    d01

---
+++ 2018/d01.rb
@@ -0,0 +1,17 @@
+require 'set'
+
+def p1(data)
+  data.inject(0, :+)
+end
+
+def p2(data)
+  current_freq = 0
+  freq_set = Set.new [0]
+  while true
+    data.each do |jump|
+      current_freq += jump
+      return current_freq if freq_set.include?(current_freq)
+      freq_set << current_freq
+    end
+  end
+end

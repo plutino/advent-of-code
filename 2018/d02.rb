1d8d25640a4a5bb6109fd897e1e651966b48fa37
Ke Zhang <ke.zhang@mycase.com>
2018-12-01 22:23:30 -0800

    d02

---
+++ 2018/d02.rb
@@ -0,0 +1,20 @@
+def p1(data)
+  counts = data.map { |str| str.split('').group_by{ |v| v }.map{ |k, v| v.size } }
+  two_counts = counts.select {|ct| ct.include?(2)}.size
+  three_counts = counts.select {|ct| ct.include?(3)}.size
+  two_counts * three_counts
+end
+
+def p2(data)
+  chared_data = data.map { |str| str.split('') }
+
+  chared_data.each_with_index do |d1, idx|
+    chared_data[(idx + 1) .. -1].each do |d2|
+      zipped = d1.zip(d2)
+      unmatched_count = zipped.select{ |e| e.first != e.last }.count
+      if unmatched_count == 1
+        return zipped.select{ |e| e.first == e.last }.map(&:first).join('')
+      end
+    end
+  end
+end
